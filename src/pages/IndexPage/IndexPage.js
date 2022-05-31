2562e7eb33070e410aa6f98368dfba372b36768d
mobaradev <mobaradev@yahoo.com>
2022-05-31 12:54:06 +0200

    changes hci2

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -2,6 +2,7 @@ import {Component} from "react";
 import SearchingBar from "./SearchingBar/SearchingBar";
 import styled from "styled-components";
 import Propositions from "./Propositions/Propositions";
+import ButtonHuge from "../../components/ButtonHuge/ButtonHuge";
 
 const Container = styled.div`
   width: 100%;
@@ -22,7 +23,22 @@ const PropositionsContainer = styled.div`
 const BottomSection = styled.div`
   width: 100%;
   height: 300px;
-  background-color: #61dafb;
+  //background-color: #61dafb;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  float: left;
+`;
+
+const BottomSectionText = styled.div`
+  width: auto;
+  height: 100%;
+  background-color: #D8D8D8;
+  padding: 20px;
+  margin-right: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   float: left;
 `;
 
@@ -42,8 +58,19 @@ class IndexPage extends Component {
                         <Propositions />
                     </div>
                 </PropositionsContainer>
-                <BottomSection>
-                    test
+                <BottomSection data-aos="fade-down">
+                    <div style={{height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                        <BottomSectionText>
+                            <div>
+                                <h2>Still nothing?</h2>
+                                <p>We also offer great system that will let you find cars suited for your needs!</p>
+                                <p>
+                                    <b>It's free ans simple</b>
+                                </p>
+                            </div>
+                        </BottomSectionText>
+                        <ButtonHuge>Fill our questionariusz</ButtonHuge>
+                    </div>
                 </BottomSection>
             </Container>
         )
